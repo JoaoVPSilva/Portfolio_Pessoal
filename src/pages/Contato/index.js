27d1a907c3d8029cf75938ab0624b51c54175e4a
João Vitor Pereira da Silva <joaovitorps2603@gmail.com>
2023-11-08 18:11:10 -0300

    ajuste pag

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,7 +1,6 @@
 
 import './style.css';
 import { GrMail } from 'react-icons/gr';
-<<<<<<< HEAD
 import { FaLinkedin } from 'react-icons/fa6';
 import { FaSquareWhatsapp } from 'react-icons/fa6';
 import { TbSquareLetterW } from 'react-icons/tb';
@@ -10,35 +9,12 @@ import img from './img/img2.png';
 function Contato(){
     return (
         <div>         
-=======
-import foto_contato from './img/contato_img.jpg'
-
-function Contato(){
-    return (
-        
-        <div>                        
-            <section class='caixa'>
-                <div class='container'> 
-                    <div class='row'>
-                        <div class="col-md-6">
-                            <div class="align-self-center" style={{marginTop: ''}}>
-                                <p id='text_home2'>Gostou do que viu? Está interessado no meu trabalho? Então entre em contato através das opções abaixo, vamos conversar para ter um trabalho de qualidade com um preço super justo!!</p>
-                            </div>                                
-                        </div>
-                        <div class="col-md-6">
-                            <img src={foto_contato} alt="" id="foto_pessoal"/>
-                        </div>
-                    </div>
-                </div>
-            </section>
->>>>>>> 636244e5d7cdec7d9b10e1bb9998506346f4616f
             <section class="caixa">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6 ">                        
                             <img src={img} alt="" id="foto_pessoal"/> 
                         </div>                        
-<<<<<<< HEAD
                         <div class="col-md-6 contatos">        
                         <br/><br/>
                         <br/><br/>                    
@@ -64,19 +40,6 @@ function Contato(){
                                 &nbsp;&nbsp;&nbsp;
                                 <a target="_blank" href="https://www.workana.com/freelancer/de73429727e80ee17817178876a1b0ee"class="text-light" style={{textDecoration: 'none'}}>Workana</a>                            
                             </div>  
-=======
-                        <div class="col-md-4 contatos">                            
-                            <h2 class='text-light'><strong>Telefone</strong><hr/></h2>
-                            <p>(35) 9 9934-3134</p>
-                        </div>
-
-                        <div class="col-md-4 contatos">                            
-                            <h2 ><strong>Redes Sociais</strong><hr/></h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            </p>
->>>>>>> 636244e5d7cdec7d9b10e1bb9998506346f4616f
                         </div>
                     </div>
                 </div>
