736664c9088b8b7ea6eecb2011541526112b6a88
João Vitor Pereira da Silva <joaovitorps2603@gmail.com>
2023-11-08 18:07:13 -0300

    ajuste contato

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,35 +1,45 @@
 
 import './style.css';
 import { GrMail } from 'react-icons/gr';
+import { FaLinkedin } from 'react-icons/fa6';
+import { FaSquareWhatsapp } from 'react-icons/fa6';
+import { TbSquareLetterW } from 'react-icons/tb';
+import img from './img/img2.png';
 
 function Contato(){
     return (
-        
-        <div>                        
+        <div>         
             <section class="caixa">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-4 ">                        
-                            <h2 class="text-light"><strong>E-mail</strong><hr/></h2>                            
-                            <GrMail style={{color: "#ecc16c",width:"30px", height: "30px"}}/>
-                            &nbsp;&nbsp;&nbsp;
-                            <a href="mailto:joaovitorps1005@gmail.com"class="text-light" style={{textDecoration: 'none'}}>joaovitorps1005@gmail.com</a>
-                            
+                        <div class="col-md-6 ">                        
+                            <img src={img} alt="" id="foto_pessoal"/> 
                         </div>                        
-                        <div class="col-md-4 contatos">                            
-                            <h2><strong>Telefone</strong><hr/></h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            </p>
-                        </div>
-
-                        <div class="col-md-4 contatos">                            
-                            <h2><strong>Redes Sociais</strong><hr/></h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            </p>
+                        <div class="col-md-6 contatos">        
+                        <br/><br/>
+                        <br/><br/>                    
+                        <h2 class="text-light"><strong>Contatos</strong><hr/></h2>     
+                            <br/>                                                   
+                            <div>
+                                <GrMail style={{color: "#ecc16c",width:"30px", height: "30px"}}/>
+                                &nbsp;&nbsp;&nbsp;
+                                <a href="mailto:joaovitorps1005@gmail.com"class="text-light" style={{textDecoration: 'none'}}>joaovitorps1005@gmail.com</a>                            
+                            </div>    
+                            <div>
+                                <FaLinkedin style={{color: "#ecc16c",width:"30px", height: "30px"}}/>
+                                &nbsp;&nbsp;&nbsp;
+                                <a target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-vitor-pereira-da-silva-84946920a/"class="text-light" style={{textDecoration: 'none'}}>Linkedin</a>                            
+                            </div>                           
+                            <div>
+                                <FaSquareWhatsapp style={{color: "#ecc16c",width:"30px", height: "30px"}}/>
+                                &nbsp;&nbsp;&nbsp;
+                                <a href=""class="text-light" style={{textDecoration: 'none'}}>(35) 9 9934-3134</a>                            
+                            </div>  
+                            <div>
+                                <TbSquareLetterW style={{color: "#ecc16c",width:"30px", height: "30px"}}/>
+                                &nbsp;&nbsp;&nbsp;
+                                <a target="_blank" href="https://www.workana.com/freelancer/de73429727e80ee17817178876a1b0ee"class="text-light" style={{textDecoration: 'none'}}>Workana</a>                            
+                            </div>  
                         </div>
                     </div>
                 </div>
