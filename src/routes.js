ff34b9926df437e074149fa601bf5dc3d85493c1
João Vitor Pereira da Silva <joaovitorps2603@gmail.com>
2023-11-09 13:23:08 -0300

    ajuste home

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ function RoutesApp(){
         <BrowserRouter>
             <Header/>
             <Routes>
-                <Route path="/" element={ <Home/> } />
+                <Route path="/home" element={ <Home/> } />
                 <Route path="/sobre" element={ <Sobre/> } />
                 <Route path="/contato" element={ <Contato/> } />                
             </Routes>
