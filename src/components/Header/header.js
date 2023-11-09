8c2cdaddf21c734eb46810647b23cfcc5845998b
João Vitor Pereira da Silva <joaovitorps2603@gmail.com>
2023-11-09 13:40:22 -0300

    ajuste

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -9,7 +9,7 @@ function Header(){
        <header class="">
             <img src={logo_joao}/> 
             <div className='menu_header'>
-                <Link to="/" class="btn_header">Home</Link>                
+                <Link to="Portfolio_Pessoal/" class="btn_header">Home</Link>                                
                 <Link to="/sobre" class="btn_header"> Sobre</Link>                        
                 <Link to="/contato" class="btn_header">Contato</Link>            
             </div>
